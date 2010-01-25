8f1aeee44369300632b85147bc80ed306ee986b1
Wysie <sohyuanchin@gmail.com>
2010-01-26 01:19:30 +0800

    Release 0.5. Dialer features fully ported over. Next up will be Contacts/Favourites, then Call Log.

--- src/com/android/contacts/TwelveKeyDialer.java
+++ src/com/android/contacts/TwelveKeyDialer.java
@@ -516,8 +516,6 @@ public class TwelveKeyDialer extends Activity implements View.OnClickListener,
         else
             prefVibrateOn = false;        
         
-        Log.d("prefVibrateOn", ""+prefVibrateOn);
-        
         updateDialer();
     }
 
@@ -1334,14 +1332,6 @@ public class TwelveKeyDialer extends Activity implements View.OnClickListener,
     	}
     	else if (prefs.getString("vm_button", "0").equals("2")) {
     		mVoicemailButton.setImageResource(R.drawable.ic_dial_action_voice_mail);
-    		
-    		/*
-            if (hasVoicemail()) {
-                mVoicemailButton.setEnabled(true);
-            } else {
-                mVoicemailButton.setEnabled(false);
-            }
-            */
     	}
     }
 
@@ -1436,6 +1426,7 @@ public class TwelveKeyDialer extends Activity implements View.OnClickListener,
     }    
     
     //Wysie: Method to check if there's any number entered
+    //Known Bug: Voicemail button is enabled on first launch. Will fix later.
     private void checkForNumber() {
         CharSequence digits = mDigits.getText();
         if ((digits == null || !TextUtils.isGraphic(digits)) && !prefs.getBoolean("dial_disable_num_check", false)) {
@@ -1443,7 +1434,16 @@ public class TwelveKeyDialer extends Activity implements View.OnClickListener,
                 mVoicemailButton.setEnabled(false);
             }
         } else {
-            mVoicemailButton.setEnabled(true);
+            if (prefs.getString("title_vm_handler", "0").equals("0")) {
+                if (hasVoicemail()) {
+                    mVoicemailButton.setEnabled(true);
+                } else {
+                    mVoicemailButton.setEnabled(false);
+                }
+    		}
+    		else {
+    		    mVoicemailButton.setEnabled(true);
+    		}
         }
     }
     
